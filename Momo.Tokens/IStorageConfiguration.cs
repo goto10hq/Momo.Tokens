e46e802c7fb11e294640c5741f7abe115229f790
Joey Frohikey <frohikey@gmail.com>
2018-08-31 15:46:34 +0200

    Add storage configuration

---
+++ Momo.Tokens/IStorageConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace Momo.Tokens
+{
+    /// <summary>
+    /// Storage configuration interface.
+    /// </summary>
+    public interface IStorageConfiguration
+    {
+        /// <summary>
+        /// Get connection string.
+        /// </summary>
+        string ConnectionString { get; }
+
+        /// <summary>
+        /// Get container.
+        /// </summary>
+        string Container { get; }
+    }
+}
---
+++ Momo.Tokens/StorageConfiguration.cs
@@ -0,0 +1,36 @@
+﻿namespace Momo.Tokens
+{
+    /// <summary>
+    /// Storage configuration.
+    /// </summary>
+    public class StorageConfiguration : IStorageConfiguration
+    {
+        /// <summary>
+        /// Get or set connection string.
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        /// Container.
+        /// </summary>
+        public string Container { get; }
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        public StorageConfiguration()
+        {
+        }
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        /// <param name="connectionString">Connection string.</param>
+        /// <param name="container">Container.</param>
+        public StorageConfiguration(string connectionString, string container)
+        {
+            ConnectionString = connectionString;
+            Container = container;
+        }
+    }
+}
