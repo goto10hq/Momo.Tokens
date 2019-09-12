f8f7612405fbdaf7fa138d12ab1610ffbc546ef7
frohikey <frohikey@gmail.com>
2019-09-12 14:34:01 +0200

    Fix redis interface

--- Momo.Tokens/IRedisConfiguration.cs
+++ Momo.Tokens/IRedisConfiguration.cs
@@ -1,7 +1,37 @@
+using System.Net;
+
 namespace Momo.Tokens
 {
-    public class IRedisConfiguration
+    public interface IRedisConfiguration
     {
-        
+        /// <summary>
+        /// Get or set host.
+        /// </summary>
+        /// <value>Host (ie. Azure Redis Caches looks like YOUR_CACHE.redis.cache.windows.net)</value>
+        string Host { get; set; }
+
+        /// <summary>
+        /// Get or set port.
+        /// </summary>
+        /// <value>Port (default value 6380).</value>   
+        int Port { get; set; }
+
+        /// <summary>
+        /// Get Dns end point.
+        /// </summary>
+        /// <returns>Dns end point.</returns>
+        DnsEndPoint EndPoint { get; }
+
+        /// <summary>
+        /// Get or set password.
+        /// </summary>
+        /// <value>Password (access key).</value>
+        string Password { get; set; }
+
+        /// <summary>
+        /// Get or set ssl.
+        /// </summary>
+        /// <value>Sll (default true).</value>
+        bool Ssl { get; set; }
     }
 }
